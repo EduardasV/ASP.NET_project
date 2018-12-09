73fd3d75f10eb2fcea00bccdaf403ef1700490c2
Eduardas <eduardas.v@hotmail.com>
2018-12-09 14:49:16 +0000

    Getting comfortable with ASP.NET
     - Created another MVC
     - understood what NuGet is and how to use
     - how to use Razor
     - What are ActionLinks
     - RenderSection
     - Web.Config
     - CustomErrors
     - Scaffolding
     - Templates
     - Packages.config

---
+++ WebApplication1/Controllers/PeopleController.cs
@@ -0,0 +1,32 @@
+﻿using WebApplication1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            PersonModel p = new PersonModel();
+            int i = 5 / p.Age;
+
+            return View();
+        }
+
+        public ActionResult ListPeople()
+        {
+            List<PersonModel> people = new List<PersonModel>();
+
+            people.Add(new PersonModel { FirstName = "Tim", LastName = "Corey", Age = 38 });
+            people.Add(new PersonModel { FirstName = "Joe", LastName = "Smith", Age = 29 });
+            people.Add(new PersonModel { FirstName = "Alice", LastName = "Bob", Age = 21 });
+
+            return View(people);
+        }
+    }
+}
